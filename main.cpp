b2413c67522db07853cbc3e6b475a0fad4ae067d
d4nielxo <dhaddad3@students.avc.edu>
2026-03-20 14:58:50 -0700

    geometry calc

--- main.cpp
+++ main.cpp
@@ -1,27 +1,47 @@
 #include <iostream>
 #include <iomanip>
-#include <cmath>
 using namespace std;
 
-int main()
-{
-    const double PI = 3.14159265358979;
-    int choice;
-    double width, height, radius, base;
-    double area;
+int main() {
+    cout << "Geometry Calculator" << endl;
+    cout << "1. Calculate the Area of a Circle" << endl;
+    cout << "2. Calculate the Area of a Rectangle" << endl;
+    cout << "3. Calculate the Area of a Triangle" << endl;
+    cout << "4. Quit" << endl;
+    cout << "Enter your choice: ";
 
-    cout << "Geometry Calculator\n";
-    cout << "\t1. Calculate the Area of Circle\n";
-    cout << "\t2. Calculate the Area of Rectangle\n";
-    cout << "\t3. Calculate the Area of Triangle\n";
-    cout << "\t4. Quit\n\n";
-    cout << "Enter your choice (1-4): ";
+    int choice;
     cin >> choice;
 
-    // TODO
+    cout << fixed << setprecision(2);
 
-    cout << left << setprecision(2) << fixed;
-    cout << "The area is " << area << endl;
+    if (choice == 1) {
+        double radius;
+        cout << "Enter the radius of the circle: ";
+        cin >> radius;
+        double area = 3.14159 * radius * radius;
+        cout << area << endl;
+    } else if (choice == 2) {
+        double length, width;
+        cout << "Enter the length of the rectangle: ";
+        cin >> length;
+        cout << "Enter the width of the rectangle: ";
+        cin >> width;
+        double area = length * width;
+        cout << area << endl;
+    } else if (choice == 3) {
+        double base, height;
+        cout << "Enter the base of the triangle: ";
+        cin >> base;
+        cout << "Enter the height of the triangle: ";
+        cin >> height;
+        double area = base * height * 0.5;
+        cout << area << endl;
+    } else if (choice == 4) {
+        cout << "Program Stopped" << endl;
+    } else {
+        cout << "Wrong number" << endl;
+    }
 
     return 0;
 }
